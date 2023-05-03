36e316952b600d90fa86659ab217e345d9bafa61
thelightone <an.i.nesterov@gmail.com>
2023-05-03 16:02:02 +0300

    -

--- DiggerTask.cs
+++ DiggerTask.cs
@@ -7,6 +7,28 @@ using System.Threading.Tasks;
 namespace Digger
 {
     //Напишите здесь классы Player, Terrain и другие.
-
+    public class Terrain : ICreature
+    {
+        public string GetImageFileName()
+        { return ""; }
+        public int GetDrawingPriority()
+        { return 0; }
+        public CreatureCommand Act(int x, int y)
+        { return null; }
+        public bool DeadInConflict(ICreature conflictedObject)
+        { return false; }
+    }
    
+    public class Player:  ICreature
+    {
+        public string GetImageFileName()
+        { return ""; }
+        public int GetDrawingPriority()
+        { return 0; }
+        public CreatureCommand Act(int x, int y)
+        { return null; }
+        public bool DeadInConflict(ICreature conflictedObject)
+        { return false; }
+    }
+
 }
